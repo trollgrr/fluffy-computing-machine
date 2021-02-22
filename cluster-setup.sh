981ddd6ade36f24f8a1d39d098e80f47bb2fa8d9
EC2 Default User <ec2-user@ip-172-31-41-224.us-west-2.compute.internal>
2021-02-22 01:51:50 +0000

    fixed some filepaths

--- cluster-setup.sh
+++ cluster-setup.sh
@@ -1,9 +1,51 @@
 #!/bin/bash
+# This should get your cluster configured and deployed, or at least tell you where it went wrong
 kind delete cluster
-kind create cluster --config=./kind-config.yaml
-sudo kubectl create -f proxy/nginx_svc.yaml
-kubectl create configmap nginx-map -f ./config/nginx.conf
-sudo kubectl create -f proxy/nginx.yaml
-sudo kubectl create -f elasticsearch/e_svc.yaml
-sudo kubectl create -f elasticsearch/elastic.yaml
-sudo kubectl create -f kibana/kibana.yaml
+if kind create cluster --config=./kind-config.yaml ; then
+	echo "Cluster created"
+else
+	echo "Cluster failed. Sorry, but check the instance setup script."
+	exit 1
+fi
+
+if kubectl create -f ./proxy/n_svc.yaml ; then
+	echo "Proxy Service Configured."
+else
+	echo "Proxy Service failed."
+	kubectl delete service nginx
+	exit 1
+fi
+
+if kubectl create configmap nginx-map --from-file=./proxy/config/nginx.conf ; then
+	echo "Config Map Configged."
+else
+	echo "Config Map Conflagration."
+	kubectl delete configmap nginx-map
+	exit 1
+fi
+if kubectl create -f proxy/nginx.yaml ; then
+	echo "Proxy Created"
+else
+	echo "Proxy Failed!"
+	kubectl delete pods nginx
+	exit 1
+fi
+
+if kubectl create -f elasticsearch/e_svc.yaml ; then
+	echo "ElasticService Created"
+else
+	echo "Inelastic services only. Better luck next time."
+	kubectl delete service elasticsearch
+	exit 1
+fi
+if kubectl create -f elasticsearch/elastic.yaml ; then
+	echo "Elastic Pods Created"
+else
+	kubectl delete pods es-cluster
+	echo "Elastic Pods failed."
+	exit 1
+
+if kubectl create -f kibana/kibana.yaml ; then
+	kubectl delete pods kibana
+	echo "kibana failed"
+	exit 1
